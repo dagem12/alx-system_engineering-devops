20234962f3c4a363cfca8ba2714cdd28d0f6af03
dagem12 <0zerocool99@gmail.com>
2025-07-06 06:21:07 -0700

    web_server

--- 0x0C-web_server/connect_server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Script: connect_server.sh
-# Purpose: SSH into a remote server using the private key ~/.ssh/school as the 'ubuntu' user
-
-SERVER_IP="my.server.ip.or.hostname"
-
-ssh -i ~/.ssh/school ubuntu@"$SERVER_IP"
-chmod +x connect_server.sh
